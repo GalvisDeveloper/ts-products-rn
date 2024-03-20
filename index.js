4c4410366b622e7209415425e0f89245c5b55435
GalvisDev <galvisdeveloper@gmail.com>
2024-03-19 21:39:11 -0500

    Se Agrega configuracion completa del stack navigator

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import ProductsApp from './src/ProductsApp';
+import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => ProductsApp);
---
+++ src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,30 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/auth/LoginScreen';
+import HomeScreen from '../screens/home/HomeScreen';
+import LoadingScreen from '../screens/loading/LoadingScreen';
+import ProductScreen from '../screens/product/ProductScreen';
+import RegisterScreen from '../screens/auth/RegisterScreen';
+
+export type RootStackParams = {
+	LoadingScreen: undefined;
+	LoginScreen: undefined;
+	RegisterScreen: undefined;
+	HomeScreen: undefined;
+	ProductScreen: { productId: string };
+};
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name='Loading' component={LoadingScreen} />
+			<Stack.Screen name='Login' component={LoginScreen} />
+			<Stack.Screen name='Register' component={RegisterScreen} />
+			<Stack.Screen name='Home' component={HomeScreen} />
+			<Stack.Screen name='Product' component={ProductScreen} />
+		</Stack.Navigator>
+	);
+};
+
+export default StackNavigator;
