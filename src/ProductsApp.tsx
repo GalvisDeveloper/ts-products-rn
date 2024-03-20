04cbcca5861477f0cbf00403a3d3907d68328d72
GalvisDev <galvisdeveloper@gmail.com>
2024-03-19 21:24:15 -0500

    Configuracion inicial del stack y estructura directorios del proyecto

---
+++ src/ProductsApp.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const ProductsApp = () => {
+	return (
+		<NavigationContainer>
+			<View>
+				<Text>Products App</Text>
+			</View>
+		</NavigationContainer>
+	);
+};
+
+export default ProductsApp;
+
+const styles = StyleSheet.create({});
