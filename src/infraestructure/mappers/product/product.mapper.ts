211fe204693f168710d6b798779b37ce2cd5d6b5
GalvisDev <galvisdeveloper@gmail.com>
2024-04-11 23:03:14 -0500

    Reduciendo logica

--- src/infraestructure/mappers/product/product.mapper.ts
+++ src/infraestructure/mappers/product/product.mapper.ts
@@ -5,17 +5,8 @@ import { ProductListResponse } from "../../interfaces/products/product.responses
 
 export class ProductMapper {
     static tesloProductToEntity(product: ProductListResponse): Product {
-
         return {
-            id: product.id,
-            title: product.title,
-            price: product.price,
-            description: product.description,
-            slug: product.slug,
-            stock: product.stock,
-            sizes: product.sizes,
-            gender: product.gender,
-            tags: product.tags,
+            ...product,
             images: product.images.map(image => `${API_URL}/files/product/${image}`) // Se obtiene la imagen de cada producto por servicio
         }
     }
