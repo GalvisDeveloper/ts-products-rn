ebd559a3f9d2f76ddb7e76ce3e7d519bff50b871
GalvisDev <galvisdeveloper@gmail.com>
2024-05-04 13:14:18 -0500

    Multipart para envio de imagenes

--- src/actions/products/update-create.ts
+++ src/actions/products/update-create.ts
@@ -19,7 +19,7 @@ export const updateCreateProduct = async (product: Partial<Product>) => {
 const updateProduct = async (product: Partial<Product>) => {
     try {
         const { id, images = [], ...rest } = product;
-        const checkedImages = prepareImages(images);
+        const checkedImages = await prepareImages(images);
 
         const { data } = await tesloApi.patch(`/products/${id}`, { ...rest, images: checkedImages });
 
@@ -31,16 +31,49 @@ const updateProduct = async (product: Partial<Product>) => {
     }
 }
 
-const prepareImages = (images: string[]) => {
-    //TODO: revisar files
-    return images.map(image => image.split('/').pop());
+const prepareImages = async (images: string[]) => {
+    const fileImages = images.filter(image => image.startsWith('file://'));
+    const currentImages = images.filter(image => !image.startsWith('file://'));
+
+    if (fileImages.length > 0) {
+        const uploadPromises = fileImages.map(uploadImage);
+        const uploadedImages = await Promise.all(uploadPromises);
+
+        currentImages.push(...uploadedImages);
+    }
+
+    return currentImages.map(image => image.split('/').pop());
+}
+
+const uploadImage = async (image: string) => {
+    try {
+        const formData = new FormData();
+
+        formData.append(`file`, {
+            uri: image,
+            name: image.split('/').pop(),
+            type: 'image/jpg'
+        });
+
+        const { data } = await tesloApi.post<{ image: string }>(`/files/product`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+
+        return data.image;
+    } catch (error) {
+        showErrorMessage(error);
+        return '';
+    }
+
 }
 
 
 const createProduct = async (product: Partial<Product>) => {
     try {
         const { id, images = [], ...rest } = product;
-        const checkedImages = prepareImages(images);
+        const checkedImages = await prepareImages(images);
 
         const { data } = await tesloApi.post(`/products`, { ...rest, images: checkedImages });
 
